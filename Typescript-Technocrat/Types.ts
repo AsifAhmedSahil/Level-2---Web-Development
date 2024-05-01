a9b778234182e3f3a8ae026815521b2659a7b19c
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-01 14:39:39 +0600

    Basic Data Types - array and tuple

---
+++ Typescript-Technocrat/Types.ts
@@ -0,0 +1,38 @@
+// basic data types
+
+// explicite type
+let firstName: string = "sahil"
+
+// implicite
+let lastName = "asif"
+
+// number
+let roll:number = 12
+
+// boolean
+let isActive = true
+
+// undefine & null
+
+let x:undefined = undefined
+let y:null = null
+
+
+// any type == it is not recomended
+let d;
+d = 12;
+d="sahil"
+d=null
+d= undefined
+
+
+// Array ===== >
+let friends:string[] = [ "sahil","asif"] 
+let rollList:number[] = [1,2,1,25,6]
+
+// Tuple ===> special Types of Array ***
+
+let Cordinates: [number,string] = [50,"sahil"]
+
+Cordinates[0] = 5
+
