5cd799da4484b6a91a4e8cbc796b03f80cd85552
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-01 15:23:31 +0600

    Destructuring In Typescript

---
+++ Typescript-Technocrat/Destructuring.ts
@@ -0,0 +1,18 @@
+// destructuring for object
+
+const user ={
+    id: 125,
+    name: {
+        firstName: "asif",
+        middleName: "Ahmed",
+        lastName: "Sahil"
+    },
+    address: "chittagong"
+}
+
+const {address, name:{middleName}} = user
+
+// array destructuring
+
+const myFriends = ["sahil","asif","ahmed"]
+const [,bestFriend] = myFriends
