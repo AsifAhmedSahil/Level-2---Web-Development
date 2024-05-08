2b02009b6b9b11872f94f231b6bfccc44221fd20
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-08 22:15:04 +0600

    question 5 solve - generics

--- Typescript-Technocrat/problem-solving(15)/5.ts
+++ Typescript-Technocrat/problem-solving(15)/5.ts
@@ -2,9 +2,86 @@
 
 interface Repository<T>{
     getAll():T[];
-    getById():T | undefined;
-    create():void;
-    update():void
-    delete():void
+    getById(id:number):T | undefined;
+    create(items:T):void;
+    update(items:T):void
+    delete(id:number):void
 }
+
+class GenericsRepository<T extends {id:number}> implements Repository<T>{
+    private items = [];
+    constructor(){
+        this.items = []
+    }
+
+    create(item: T): void {
+        this.items.push(item)
+    }
+    getAll(): T[] {
+        return this.items
+    }
+    getById(id:number): T | undefined {
+        return this.items.find((item:T) => item.id === id)
+        
+    }
+
+    update(item: T): void {
+        const index = this.items.findIndex(
+            (existingItem:T) =>existingItem.id === item.id
+        )
+        if(index !== -1){
+            this.items[index] = item
+        }
+    }
+
+    delete(id: number): void {
+        const index = this.items.findIndex(
+            (existingItem:T) =>existingItem.id === id
+        )
+        if(index !== -1){
+            this.items.splice(index,1)
+        }
+    }
+
+}
+
+// create instance
+
+interface IProgrammer {
+    id:number;
+    name:string;
+    email:string;
+}
+
+const programmerRepository = new GenericsRepository<IProgrammer>();
+
+programmerRepository.create({
+    id:1,
+    name:'sahil',
+    email:'sahil@gmail.com'
+})
+programmerRepository.create({
+    id:2,
+    name:'Asif',
+    email:'sahil@gmail.com'
+})
+programmerRepository.create({
+    id:3,
+    name:'Ahmed',
+    email:'sahil@gmail.com'
+})
+
+
+programmerRepository.update({id:1,name:'sanji',email:"sanji@gmail.com"})
+
+programmerRepository.delete(3)
+
+
+const allUser = programmerRepository.getAll()
+console.log(allUser)
+
+// const oneUser = programmerRepository.getById(1)
+// console.log(oneUser)
+
+
 
