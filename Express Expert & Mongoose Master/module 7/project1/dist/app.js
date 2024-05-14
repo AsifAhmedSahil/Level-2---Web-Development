e59268d5a06a8096a059ce397d0ecac0ae44a423
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-14 13:12:18 +0600

    Routing In Express.Js

--- Express Expert & Mongoose Master/module 7/project1/dist/app.js
+++ Express Expert & Mongoose Master/module 7/project1/dist/app.js
@@ -13,6 +13,27 @@ const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
     next();
 };
+// router in express
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+// post request for create user
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: "create user successfully"
+    });
+});
+// post request for course user
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: "create user successfully"
+    });
+});
 app.get('/', logger, (req, res) => {
     res.send('Hello sahil world!');
 });
--- Express Expert & Mongoose Master/module 7/project1/src/app.ts
+++ Express Expert & Mongoose Master/module 7/project1/src/app.ts
@@ -11,6 +11,41 @@ const logger = (req:Request,res:Response,next:NextFunction){
   next()
 }
 
+// router in express
+
+const userRouter = express.Router()
+const courseRouter = express.Router()
+
+app.use("/api/v1/users",userRouter)
+app.use("/api/v1/courses",courseRouter)
+
+// post request for create user
+userRouter.post("/create-user",(req:Request,res:Response) =>{
+  const user = req.body;
+  console.log(user)
+
+  res.json({
+    success: "create user successfully"
+
+  })
+})
+
+// post request for course user
+
+courseRouter.post("/create-course",(req:Request,res:Response) =>{
+  const course = req.body;
+  console.log(course)
+
+  res.json({
+    success: "create user successfully"
+
+  })
+})
+
+
+
+
+
 app.get('/',logger, (req:Request, res:Response) => {
   res.send('Hello sahil world!')
 })
