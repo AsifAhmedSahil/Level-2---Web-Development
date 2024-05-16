3a3926e80848e17b1441725b214145ec08cc66b3
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-16 18:28:40 +0600

    Install Prettier,Ts-Node-Dev,Fix Formatting Issues

--- Express_Expert_Mongoose_Master/module 8/project/src/app.ts
+++ Express_Expert_Mongoose_Master/module 8/project/src/app.ts
@@ -1,19 +1,15 @@
-
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-const app : Application = express()
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
 // const port = 3000
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
 
-
-app.get('/', (req:Request, res:Response) => {
-    var a = 10
-  res.send(a)
-})
-
-
+  res.send(a);
+});
 
 export default app;
