f4e3c7c873588ac96a8794b46603e52133b22f2d
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-25 19:28:46 +0600

    implement higher order function into user controller and student controller

--- Express_Expert_Mongoose_Master/module 8/project/src/modules/student/student.controller.ts
+++ Express_Expert_Mongoose_Master/module 8/project/src/modules/student/student.controller.ts
@@ -1,23 +1,18 @@
 /* eslint-disable no-unused-vars */
-import {   NextFunction, Request, RequestHandler, Response, } from 'express';
 import { studentServices } from './student.service';
+import catchAsync from '../../utils/catchAsync';
 // import studentValidationSchema from './student.validaion';
 
 // import studentValidationSchema from './student.validation';
 // import {z} from "zod"
 
 
-// **************higher order function***************
-const catchAsync = (fn : RequestHandler) =>{
-    return (req:Request,res:Response,next:NextFunction) =>{
-        Promise.resolve(fn(req,res,next)).catch((err) => next(err))
-    }
-}
+
 
 
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getAllStudents  = catchAsync(async (req,res ,next) =>{
+const getAllStudents  = catchAsync(async (req,res ) =>{
     
         const result = await studentServices.getAllStudentsFromDB();
         res.status(200).json({
@@ -29,36 +24,29 @@ const getAllStudents  = catchAsync(async (req,res ,next) =>{
 )
 
 // get single student from db
-const getSingleStudent : RequestHandler = async (req,res,next) =>{
-    try {
-        const {studentId}  = req.params
-        const result = await studentServices.getSingleStudentFromDB(studentId);
-        res.status(200).json({
-            success: true,
-            message: 'Student is retrived Successfully',
-            data: result,
-          });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error ) {
-      next(error)
-        
-    }
-}
-const deleteSingleStudent : RequestHandler = async (req,res,next) =>{
-    try {
-        const {studentId}  = req.params
-        const result = await studentServices.deleteStudentFromDB(studentId);
-        res.status(200).json({
-            success: true,
-            message: 'Student is deleted Successfully',
-            data: result,
-          });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error ) {
-      next(error)
-        
-    }
-}
+const getSingleStudent  = catchAsync(async (req,res) =>{
+    
+    const {studentId}  = req.params
+    const result = await studentServices.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+        success: true,
+        message: 'Student is retrived Successfully',
+        data: result,
+      });
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+})
+const deleteSingleStudent  = catchAsync(async (req,res) =>{
+    
+    const {studentId}  = req.params
+    const result = await studentServices.deleteStudentFromDB(studentId);
+    res.status(200).json({
+        success: true,
+        message: 'Student is deleted Successfully',
+        data: result,
+      });
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+})
 
 export const studentController = {
     
--- Express_Expert_Mongoose_Master/module 8/project/src/modules/user/user.controller.ts
+++ Express_Expert_Mongoose_Master/module 8/project/src/modules/user/user.controller.ts
@@ -1,27 +1,24 @@
-import {  RequestHandler,  } from "express";
+
 import { UserServices } from "./user.service";
+import catchAsync from "../../utils/catchAsync";
 
 
-const createStudent : RequestHandler = async (req,res,next) => {
-    try {
-      const {password, student:studentData} = req.body;
-  
-      // using zod validation
-    //   const zodParsedData = studentValidationSchema.parse(studentData)
-  
-      const result = await UserServices.createStudentIntoDB(password,studentData);
-      
-      
-      res.status(200).json({
-        success: true,
-        message: 'Student Create Successfully',
-        data: result,
-      });
-    } catch (error ) {
-      next(error)
-    }
-  };
+const createStudent  = catchAsync(async (req,res) => {
+    
+  const {password, student:studentData} = req.body;
+  // using zod validation
+//   const zodParsedData = studentValidationSchema.parse(studentData)
 
+  const result = await UserServices.createStudentIntoDB(password,studentData);
+
+  res.status(200).json({
+    success: true,
+    message: 'Student Create Successfully',
+    data: result,
+  });
+
+}
+)
 
   export const userControllers = {
     createStudent
