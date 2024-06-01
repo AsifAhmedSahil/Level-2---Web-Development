beb40c90a714e6501bc04cfffce1c2131b17bcc6
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-06-01 10:28:34 +0600

    Convert Zod Error

--- Express_Expert_Mongoose_Master/module 8/project/src/middlewares/globalErrorHandler.ts
+++ Express_Expert_Mongoose_Master/module 8/project/src/middlewares/globalErrorHandler.ts
@@ -1,14 +1,55 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable prefer-const */
 
-import  { NextFunction, Request, Response} from 'express';
+import  { ErrorRequestHandler, } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../app/config';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-unused-vars, @typescript-eslint/no-unused-vars
-const globalErrorHandler = (err:any,req: Request, res: Response, next:NextFunction) =>{
-    const statusCode = err.statusCode || 500;
-    const message =  err.message || "something went wrong!"
-  
+const globalErrorHandler : ErrorRequestHandler = (err,req, res, next) =>{
+    let statusCode = err.statusCode || 500;
+    let message =  err.message || "something went wrong!"
+    
+    let errorSources : TErrorSource = [
+      {
+        path:"",
+        message: "Something went wrong"
+      }
+    ]
+
+    const handleZodError = ( err : ZodError ) =>{
+      const errorSources :TErrorSource = err.issues.map((issue:ZodIssue) => {
+        return {
+          path: issue?.path[issue.path.length - 1],
+          message:issue.message
+        }
+      })
+      const statusCode = 400 ;
+      return{
+        statusCode,
+        message: "Zod validation error",
+        errorSources
+      }
+    }
+
+    if(err instanceof ZodError){
+      const simplifiedError = handleZodError(err)
+      statusCode = simplifiedError.statusCode,
+      message = simplifiedError.message,
+      errorSources = simplifiedError.errorSources
+       
+      
+     
+    }
+
+
+    
     return res.status(statusCode).json({
       success:false,
       message,
-      error:err
+      // amiError:err,
+      errorSources,
+      stack: config.node_env === "development" ? err?.stack : null
     })
   }
 
--- Express_Expert_Mongoose_Master/module 8/project/src/server.ts
+++ Express_Expert_Mongoose_Master/module 8/project/src/server.ts
@@ -5,6 +5,7 @@ import config from './app/config';
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
+    console.log("mongodb connected")
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
