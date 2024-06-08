1580f86cd7a065e44abe7f9021c8b0b09c6e8cda
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-06-08 22:08:58 +0600

    Implement Business Logic Validation In Auth Service.

--- Express_Expert_Mongoose_Master/module 8/project/src/modules/auth/auth.service.ts
+++ Express_Expert_Mongoose_Master/module 8/project/src/modules/auth/auth.service.ts
@@ -1,10 +1,36 @@
 // import { User } from "../user/user.model";
+import httpStatus from "http-status";
+import AppError from "../../error/AppError";
+import { User } from "../user/user.model";
 import { TLoginUser } from "./auth.interface";
+import bcrypt from "bcrypt"
 
 
 const loginUser = async(payload:TLoginUser) =>{
 
-    console.log(payload)
+    // check user is exist in database or not
+    const isUserExists = await User.findOne({id: payload?.id})
+    if(!isUserExists){
+        throw new AppError(httpStatus.NOT_FOUND,"This user is not exist in database")
+    }
+
+    // check user is already deleted or not
+    const isDeleted = isUserExists?.isDeleted
+    if(isDeleted){
+        throw new AppError(httpStatus.FORBIDDEN,"This user is already deleted!")
+    }
+
+    // check user is blocked or not
+    const userStatus = isUserExists?.status
+    if(userStatus === 'blocked'){
+        throw new AppError(httpStatus.FORBIDDEN,"This user is already blocked!")
+    }
+
+    // check the password is matched or not
+    const isPasswordMatched = await bcrypt.compare(payload?.password,isUserExists?.password)
+    
+
+    
     return {}
 
 }
