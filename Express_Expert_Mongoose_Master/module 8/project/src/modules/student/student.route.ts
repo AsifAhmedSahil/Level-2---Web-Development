7122477d197a3bc8f1846994e1718b5588c65299
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-06-16 13:30:57 +0600

    check token and access security

--- Express_Expert_Mongoose_Master/module 8/project/src/modules/student/student.route.ts
+++ Express_Expert_Mongoose_Master/module 8/project/src/modules/student/student.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { studentController } from "./student.controller";
 import validationSchema from "../../middlewares/validationSchema";
 import { studentValidations } from "./student.validaion";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ const router = express.Router();
 
 
 router.get('/',studentController.getAllStudents)
-router.get('/:studentId',studentController.getSingleStudent)
+router.get('/:studentId',auth('admin','faculty'),studentController.getSingleStudent)
 router.patch('/:studentId',validationSchema(studentValidations.updateStudentValidationSchema),studentController.updateSingleStudent)
 router.delete('/:studentId',studentController.deleteSingleStudent)
 
