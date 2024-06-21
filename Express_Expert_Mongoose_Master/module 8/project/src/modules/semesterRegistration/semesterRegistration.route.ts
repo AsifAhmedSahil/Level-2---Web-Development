e0f04c592aa5d4780275f2d9d42583d8fad3561b
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-06-21 15:17:33 +0600

    Modify Academic Semester Data _ Create Semester Registration

--- Express_Expert_Mongoose_Master/module 8/project/src/modules/semesterRegistration/semesterRegistration.route.ts
+++ Express_Expert_Mongoose_Master/module 8/project/src/modules/semesterRegistration/semesterRegistration.route.ts
@@ -4,16 +4,18 @@ import validationSchema from "../../middlewares/validationSchema";
 import { semesterRegistrationValidation } from "./semesterRegistration.validation";
 
 import { semesterRegistrationControllers } from "./semesterRegistration.controller";
+import auth from "../../middlewares/auth";
+import { USER_ROLE } from "../user/user.constants";
 
 
 
 const router = express.Router();
 
 
-router.post("/create-semester-registration",validationSchema(semesterRegistrationValidation.createSemesterRegistrationValidationSchema),semesterRegistrationControllers.createSemesterRegistration)
-router.get("/",semesterRegistrationControllers.getAllSemesterRegistration)
-router.get("/:id",semesterRegistrationControllers.getSingleSemesterRegistration)
-router.patch("/:id",validationSchema(semesterRegistrationValidation.updateSemesterRegistrationValidationSchema),semesterRegistrationControllers.updateSemesterRegistration)
+router.post("/create-semester-registration",auth(USER_ROLE.superAdmin,USER_ROLE.admin),validationSchema(semesterRegistrationValidation.createSemesterRegistrationValidationSchema),semesterRegistrationControllers.createSemesterRegistration)
+router.get("/",auth(USER_ROLE.superAdmin,USER_ROLE.admin,USER_ROLE.faculty,USER_ROLE.student),semesterRegistrationControllers.getAllSemesterRegistration)
+router.get("/:id",auth(USER_ROLE.superAdmin,USER_ROLE.admin,USER_ROLE.faculty,USER_ROLE.student),semesterRegistrationControllers.getSingleSemesterRegistration)
+router.patch("/:id",auth(USER_ROLE.superAdmin,USER_ROLE.admin),validationSchema(semesterRegistrationValidation.updateSemesterRegistrationValidationSchema),semesterRegistrationControllers.updateSemesterRegistration)
 
 
 export const semesterRegistrationRoute  = router;
