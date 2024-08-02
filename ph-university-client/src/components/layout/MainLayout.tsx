33ba7516736c38e1109f961a1fa5e4be63a2a071
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-02 20:01:14 +0600

    Login redirect and logout

--- ph-university-client/src/components/layout/MainLayout.tsx
+++ ph-university-client/src/components/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
 // import {
 //   UploadOutlined,
 //   UserOutlined,
@@ -8,15 +8,25 @@ import { Layout } from 'antd';
 import {  Outlet } from 'react-router-dom';
 
 import Sidebar from './Sidebar';
+import { useAppDispatch } from '../../redux/hooks';
+import { logout } from '../../redux/features/auth/authSlice';
+import { toast } from 'sonner';
 
 const { Header, Content } = Layout;
 
+
 const MainLayout = () => {
+  const dispatch = useAppDispatch()
+
+const handleLogout = () =>{
+  toast.success("log out user successfully")
+  dispatch(logout())
+}
   return (
     <Layout style={{ height: '100vh' }}>
       <Sidebar/>
       <Layout>
-        <Header style={{ padding: 0 }} />
+        <Header style={{ padding: 0 }} ><Button onClick={handleLogout}>Log Out</Button></Header>
         <Content style={{ margin: '24px 16px 0' }}>
           <div
             style={{
--- ph-university-client/src/pages/Login.tsx
+++ ph-university-client/src/pages/Login.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
 
 
 
 const Login = () => {
+  const navigate = useNavigate()
   const dispatch = useAppDispatch()
   const {register,handleSubmit} = useForm(
     {
@@ -19,22 +23,29 @@ const Login = () => {
     }
   )
 
-  const [login,{error}] = useLoginMutation()
+  const [login] = useLoginMutation()
   
-  console.log("error",error)
+  
 
-  const onSubmit = async (data:any) =>{
-    console.log(data)
-    const userInfo = {
-      id: data.id,
-      password:data.password
+  const onSubmit = async (data : FieldValues) =>{
+    const toastId = toast.loading("login user...")
+    // console.log(data)
+    try {
+      const userInfo = {
+        id: data.id,
+        password:data.password
+      }
+      const res = await login(userInfo).unwrap()
+      const user = verifyToken(res.data.accessToken) as TUser
+     
+      console.log(user)
+      
+      dispatch(setUser({user:user , token:res.data.accessToken}))
+      toast.success("Logged In User Succesfully!",{id:toastId , duration:2000})
+      navigate(`/${user.role}/dashboard`)
+    } catch (error) {
+      toast.error("Something Went Wrong!",{id:toastId , duration:2000})
     }
-    const res = await login(userInfo).unwrap()
-    const user = verifyToken(res.data.accessToken)
-   
-    console.log(user)
-    
-    dispatch(setUser({user:user , token:res.data.accessToken}))
 
 
   }
