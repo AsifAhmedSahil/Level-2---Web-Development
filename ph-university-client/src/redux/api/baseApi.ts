389804db4597825cfc3f1cc37e9739d5e1659b92
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-03 00:46:03 +0600

    Retrieve new access token using refresh token

--- ph-university-client/src/redux/api/baseApi.ts
+++ ph-university-client/src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery(
     { 
@@ -20,6 +21,27 @@ const baseQuery = fetchBaseQuery(
  const baseQueryWithRefreshToken = async (args,api,extraOptions) =>{
     const result = await baseQuery(args,api,extraOptions)
     console.log(result)
+    if(result.error?.status === 401){
+        // * send refresh token
+        console.log("send refresh token!")
+        const res = await fetch('http://localhost:3000/api/v1/auth/refresh-token',{
+            method: 'POST',
+            credentials: "include"
+        })
+
+        const data = await res.json();
+        console.log(data)
+
+        const user = (api.getState() as RootState).auth.user
+
+        api.dispatch(
+            setUser({
+                user,
+                token:data.data.accessToken
+            })
+        )
+    }
+    return result
  }
 
 export const baseApi = createApi({
