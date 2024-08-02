024bb583bdbbbe9b5d0c9433f0d3886bfd022c75
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-03 00:08:56 +0600

    Setting up baseQuery to send accessToken with each request

--- ph-university-client/src/redux/api/baseApi.ts
+++ ph-university-client/src/redux/api/baseApi.ts
@@ -1,16 +1,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
+const baseQuery = fetchBaseQuery(
+    { 
+        baseUrl: 'http://localhost:3000/api/v1' ,
+        credentials: 'include',
+        prepareHeaders: (headers,{getState}) =>{
+            const token = (getState() as RootState).auth.token
+            if(token){
+                headers.set('authorization',`${token}`)
+            }
+
+            return headers
+        }
+    },
+    //! here credentials include means access cookie info from cookies**
+    
+ )
 
 export const baseApi = createApi({
     reducerPath:'baseApi',
-    baseQuery: fetchBaseQuery(
-        { 
-            baseUrl: 'http://localhost:3000/api/v1' ,
-            credentials: 'include'
-        },
-        //! here credentials include means access cookie info from cookies**
-        
-     ),
-    
+    baseQuery: baseQuery,
     endpoints: () =>({ })
 })
