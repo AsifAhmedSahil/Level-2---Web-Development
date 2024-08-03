4fe570f8de20b1a365295df5c6f075ff4588c854
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-03 16:03:33 +0600

    Setting up the next steps for user management

---
+++ ph-university-client/src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -0,0 +1,20 @@
+import { FieldValues, SubmitHandler } from "react-hook-form"
+import PHForm from "../../../components/form/PHForm"
+import PHInput from "../../../components/form/PHInput"
+import { Button } from "antd"
+
+
+const CreateAcademicSemester = () => {
+    const onSubmit : SubmitHandler<FieldValues> = (data) =>{
+        console.log(data)
+
+    }
+  return (
+    <PHForm onSubmit={onSubmit} >
+        <PHInput type="text" name="name"/>
+        <Button htmlType="submit">Submit</Button>
+    </PHForm>
+  )
+}
+
+export default CreateAcademicSemester
