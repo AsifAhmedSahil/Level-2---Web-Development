64f55b3188f53b9e324959351c72f4a743f52b44
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-13 10:50:20 +0600

    cart reducer and slice setup

--- Redux/e-shadhin-redux-recap/src/redux/feature/cartSlice.ts
+++ Redux/e-shadhin-redux-recap/src/redux/feature/cartSlice.ts
@@ -1,12 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '../store'
+import { products } from '../../data/products'
 
 
 
 // Define the initial state using that type
 const initialState = {
-  value: 0,
+  products: [] as any,
+  selestedItems:0,
+  totalPrice:0,
+  tax:0,
+  taxRate:0.1,
+  grandTotal:0
+
 }
 
 export const cartSlice = createSlice({
@@ -14,11 +21,17 @@ export const cartSlice = createSlice({
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-   
+    addToCart: (state,action) =>{
+      const isExist = state.products.find(product => product.id === action.payload.id)
+
+      if(!isExist){
+        state.products.push({...action.payload,quantity:1})
+      }
+    }
   },
 })
 
-export const {} = cartSlice.actions
+export const {addToCart} = cartSlice.actions
 
 
 export default cartSlice.reducer
