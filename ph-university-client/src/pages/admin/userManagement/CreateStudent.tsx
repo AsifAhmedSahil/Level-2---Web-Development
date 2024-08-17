9763c4b99bcaa121ab101e6bef7b951b7ecd4bc2
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-18 00:22:42 +0600

    Create Student basic form structure

--- ph-university-client/src/pages/admin/userManagement/CreateStudent.tsx
+++ ph-university-client/src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,58 +1,82 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
-import PHForm from "../../../components/form/PHForm"
+import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button } from "antd";
+import { Button, Col, Divider, Row } from "antd";
 
 const studentSummyData = {
-  "password":"student123",
-  "student": {
-      "name": {
-          "firstName": "Asif",
-          "middleName": "mahmud",
-          "lastName": "sahil"
-      },
-      "gender": "male",
-      "dateOfBirth": "1995-03-15",
-      "bloodGroup": "A+",
+  password: "student123",
+  student: {
+    name: {
+      firstName: "Asif",
+      middleName: "mahmud",
+      lastName: "sahil",
+    },
+    gender: "male",
+    dateOfBirth: "1995-03-15",
+    bloodGroup: "A+",
 
-      "email": "towsif.doe@example.com",
-      "contact": "1234567890",
-      "emergenceContactNo": "0987654321",
-      "presentAddress": "123 Main St, City, Country",
-      "permanentAddress": "456 Elm St, City, Country",
+    email: "towsif.doe@example.com",
+    contact: "1234567890",
+    emergenceContactNo: "0987654321",
+    presentAddress: "123 Main St, City, Country",
+    permanentAddress: "456 Elm St, City, Country",
 
-      "guirdian": {
-          "fatherName": "Michael Smith",
-          "fatherOccupation": "Engineer",
-          "fatherNo": "9876543210",
-          "motherName": "Emily Smith",
-          "motherNo": "1234567890",
-          "motherOccupation": "Teacher"
-      },
+    guirdian: {
+      fatherName: "Michael Smith",
+      fatherOccupation: "Engineer",
+      fatherNo: "9876543210",
+      motherName: "Emily Smith",
+      motherNo: "1234567890",
+      motherOccupation: "Teacher",
+    },
 
-      "localGuirdian": {
-          "name": "Jane Doe",
-          "address": "789 Oak St, City, Country",
-          "contact": "2345678901"
-      },
-      
-      "photoUrl": "https://example.com/john_doe_photo.jpg",
-      "academicSemester": "6674f3db444d879394103420",
-      "academicDepartment": "6674f2b4444d87939410341b",
-      "isActive": "active"
-  }
-}
+    localGuirdian: {
+      name: "Jane Doe",
+      address: "789 Oak St, City, Country",
+      contact: "2345678901",
+    },
+
+    photoUrl: "https://example.com/john_doe_photo.jpg",
+    academicSemester: "6674f3db444d879394103420",
+    academicDepartment: "6674f2b4444d87939410341b",
+    isActive: "active",
+  },
+};
 const CreateStudent = () => {
-  const onSubmit : SubmitHandler<FieldValues> = (data) =>{
-    console.log(data)
-  }
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
 
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" label="Name"/>
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Row>
+      <Col span={24}>
+        <PHForm onSubmit={onSubmit}>
+          <Divider>Personal Info</Divider>
+          <Row gutter={8}>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="name.firstName" label="First Name" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="name.middleName" label="Middle Name" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="name.lastName" label="Last Name" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="gender" label="Gender" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="dateOfBirth" label="DOB" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="bloodGroup" label="Blood Group" />
+            </Col>
+          </Row>
+            <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Row>
   );
 };
 
-export default CreateStudent
+export default CreateStudent;
