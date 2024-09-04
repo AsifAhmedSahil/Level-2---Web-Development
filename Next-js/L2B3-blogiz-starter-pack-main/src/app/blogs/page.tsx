f497f12a48bd6d7d374919f1daa4475d0b974357
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-05 00:36:37 +0600

    Displaying Blog Data Using Redux

--- Next-js/L2B3-blogiz-starter-pack-main/src/app/blogs/page.tsx
+++ Next-js/L2B3-blogiz-starter-pack-main/src/app/blogs/page.tsx
@@ -1,22 +1,28 @@
+/* eslint-disable @next/next/no-async-client-component */
+"use client"
 import BlogCard from "@/components/ui/BlogCard"
+import { useGetBlogsQuery } from "@/redux/api/baseApi"
 import { Blog } from "@/types"
 
 
-const page =async () => {
-    const res = await fetch("http://localhost:5000/blogs",{
-        cache:"no-store"
-      })
-      const blogs = await res.json()
+const BlogPage =async () => {
+    // const res = await fetch("http://localhost:5000/blogs",{
+    //     cache:"no-store"
+    //   })
+    //   const blogs = await res.json()
+
+      const {data:blogs} = useGetBlogsQuery("")
+
   return (
     <div>
         <h1 className="text-4xl font-bold text-center my-5">All Blogs From Blogiz</h1>
         <div className="grid grid-cols-3 gap-4 w-[90%] mx-auto my-5">
             {
-                blogs.map((blog:Blog) => <BlogCard blog={blog} key={blog.id}/>)
+                blogs?.map((blog:Blog) => <BlogCard blog={blog} key={blog.id}/>)
             }
         </div>
     </div>
   )
 }
 
-export default page
+export default BlogPage
