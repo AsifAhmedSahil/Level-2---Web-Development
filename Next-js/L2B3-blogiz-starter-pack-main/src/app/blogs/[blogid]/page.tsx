e00dafe8fb66ade83800fa565388eb06054e904d
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-04 22:51:06 +0600

    Fetch dynamic data using generateStaticParams() for SSG

--- Next-js/L2B3-blogiz-starter-pack-main/src/app/blogs/[blogid]/page.tsx
+++ Next-js/L2B3-blogiz-starter-pack-main/src/app/blogs/[blogid]/page.tsx
@@ -1,10 +1,19 @@
 import BlogDetails from "@/components/ui/BlogDetails"
+import { Blog } from "@/types";
 
 interface BlogId {
     params:{
         blogid: string
     }
 }
+
+export const generateStaticParams = async () =>{
+    const res = await fetch("http://localhost:5000/blogs");
+    const blogs = await res.json()
+    return blogs.slice(0,3).map((blog: Blog) =>{
+        blogid: blog.id
+    })
+} 
 
 const BlogDetailsPage = async ({params }:BlogId) => {
     console.log(params)
