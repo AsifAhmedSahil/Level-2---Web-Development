8cc961778ccca996ee007b070a3972c537d51121
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-06 09:45:33 +0600

    Implement GitHub authentication system

--- Next-js/next-auth-module/nextjs-authentication/src/utils/nextOptions.ts
+++ Next-js/next-auth-module/nextjs-authentication/src/utils/nextOptions.ts
@@ -1,10 +1,14 @@
 import  { NextAuthOptions } from "next-auth"
+import GitHubProvider from "next-auth/providers/github";
 
 
 export const authOptions: NextAuthOptions = {
   
   providers: [
-    
+    GitHubProvider({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string
+      })
   ],
 }
 
